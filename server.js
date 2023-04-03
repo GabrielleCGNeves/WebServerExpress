f35e031e8bed27670879603b14d1c68d72ed2ab2
unknown <gabriellecarvalhoneves@gmail.com>
2023-04-03 15:39:10 -0300

    :sparkles: imported file path

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./app.js');
 
-app.set('port', 3000);
+require('dotenv').config({path:'variable.env'});
+
+app.set('port', process.env.port);
 const server = app.listen(app.get('port'), () =>{
     console.log("Servidor executado no end: " + server.address())
 })
